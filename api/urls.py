f18261f6e7963ade539f7903220a1dbe73e96456
PeaceTem <olumidejonathan10@gmail.com>
2024-10-02 10:37:56 +0100

    final model push

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PredictView
+from .views import PredictView, my_json_view
 
 urlpatterns = [
-    path("predict", PredictView.as_view(), name="predict"),
+    path("predict", PredictView.as_view(), name="predict-home"),
+    path("predict/final_grade", my_json_view, name="predict"),
 ]
 
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views import View
 import joblib
+import json
 import os
 import numpy as np
 import pandas as pd
@@ -30,6 +31,9 @@ class PredictView(View):
         }
         return render(request, 'api/predict_info.html', context)
     
+
+
+    # use a function-based view
     # @csrf_exempt
     def post(self, request):
         """
@@ -38,8 +42,12 @@ class PredictView(View):
         """
         try:
             # Get the input data from the POST request
-            data = request.POST.dict()  # if form data is used
-            print(data)
+            # print(request.POST.items())
+
+            for key, value in request.POST.items():
+                print(f"{key}: {value}")
+            data = self.request.POST.dict()  # if form data is used
+            # print(data)
             # Alternatively, if you are expecting JSON data, use:
             # import json
             # data = json.loads(request.body)
@@ -100,4 +108,77 @@ class PredictView(View):
 
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+
+
+
+
+@csrf_exempt
+def my_json_view(request):
+    if request.method == 'POST':
+        # Get JSON data from the request body
+        try:
+            data = json.loads(request.body)
+
+            input_data = [[
+                int(data.get('Hours_Studied', '10')), #
+
+                int(data.get('Attendance', '75')), #
+                int(data.get('Sleep_Hours', '7')), #
+                1,
+                1,
+                calc_cgpa(data.get('Previous_cgpa', '75')), #
+                2, 
+                2,
+                family_income(data.get('Family_Income', 'Medium')), #Low: 1, Medium: 2, High: 0
+                1,
+                1,
+                2,
+                0,
+                2,
+                0,
+                0,
+                1,
+                1,
+                Learning_Disabilities(data.get('Learning_Disabilities', 'No')), # No: 0, Yes: 1
+                # Add more features as required
+            ]]
+
+            feature_names = ['Hours_Studied',
+                            'Attendance',
+                            'Sleep_Hours', 
+                            'Tutoring_Sessions', 
+                            'Physical_Activity', 
+                            'Previous_cgpa', 
+                            'Access_to_Resources_Encoded', 
+                            'Motivation_Level_Encoded', 
+                            'Family_Income_Encoded', 
+                            'Teacher_Quality_Encoded', 
+                            'School_Type_Encoded', 
+                            'Peer_Influence_Encoded', 
+                            'Parental_Education_Level_Encoded', 
+                            'Distance_from_Home_Encoded', 
+                            'Gender_Encoded', 
+                            'Parental_Involvement_Encoded', 
+                            'Extracurricular_Activities_Encoded', 
+                            'Internet_Access_Encoded', 
+                            'Learning_Disabilities_Encoded']
+            
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+        
+
+        
+        X_test_manual = pd.DataFrame(input_data, columns=feature_names) # reduce score to 0 to 1
+        # Use the model to make a prediction
+        prediction = model.predict(X_test_manual).tolist()[0]
+
+        # Create a response
+        print("Prediction: ", prediction)
+        response_data = {
+            'grade': prediction,
+        }
+        return JsonResponse(response_data)
+    
+    # If it's not a POST request, return an error
+    return JsonResponse({'error': 'POST request required'}, status=405)
 
